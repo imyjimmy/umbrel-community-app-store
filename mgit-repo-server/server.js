276c1be3a25eb71f2403d9bc0b8867e635309e9e
imyjimmy <imyjimmy@gmail.com>
2025-06-07 19:39:04 -0500

    slight tweak to port number, remove 3003 from web environment in docker-compose

--- mgit-repo-server/server.js
+++ mgit-repo-server/server.js
@@ -910,7 +910,7 @@ app.get('*', (req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Access the application at http://localhost:${PORT}`);
